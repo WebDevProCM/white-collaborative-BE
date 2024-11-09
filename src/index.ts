4b1289f9605168dee9e50b1c501f62186cf2049b
Chamod <webdevprocm@gmail.com>
2024-11-09 17:35:34 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+server.listen(3001, () =>{
+    console.log("server is runnig");
+})
